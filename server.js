cbb70ab2c11eb99e1537a3959288344ac422da30
Haim Yeshurun <haimyess@gmail.com>
2022-09-20 17:15:31 +0300

    testing deploy

--- server.js
+++ server.js
@@ -14,9 +14,25 @@ dotenv.config();
 
 const cors = require("cors");
 app.use(cors());
+
+const path = require("path");
 // Listenting to the port 5000 in localhost
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`listen on port ${process.env.PORT || 8080}`);
+// app.listen(process.env.PORT || 8080, () => {
+//   console.log(`listen on port ${process.env.PORT || 8080}`);
+// });
+
+const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    req.sendFile(path.resolve(_dirname, "build", "index.html"));
+  });
+}
+
+app.listen(port, (err) => {
+  if (err) return console.log(err);
+  console.log("Server running on port:", port);
 });
 
 app.use(express.urlencoded({ extended: true }));
